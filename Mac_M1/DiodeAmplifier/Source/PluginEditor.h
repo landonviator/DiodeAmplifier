cc429a37982f65042667948b5de9a35b48ddb91e
Landon <33662627+landonviator@users.noreply.github.com>
2021-05-18 18:37:53 -0500

    Mac M1 builds added
    4K is squashed now

---
+++ Mac_M1/DiodeAmplifier/Source/PluginEditor.h
@@ -0,0 +1,53 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+#include "PluginProcessor.h"
+#include "ViatorDial.h"
+
+//==============================================================================
+/**
+*/
+class DiodeAmplifierAudioProcessorEditor  : public juce::AudioProcessorEditor
+{
+public:
+    DiodeAmplifierAudioProcessorEditor (DiodeAmplifierAudioProcessor&);
+    ~DiodeAmplifierAudioProcessorEditor() override;
+
+    //==============================================================================
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+private:
+    juce::Slider inputSlider, driveSlider, lowSlider, midSlider, highSlider, outputSlider;
+    std::vector<juce::Slider*> sliders;
+    std::unique_ptr <juce::AudioProcessorValueTreeState::SliderAttachment> inputSliderAttach, driveSliderAttach, lowSliderAttach, midSliderAttach, highSliderAttach, outputSliderAttach;
+        
+    juce::GroupComponent windowBorder;
+        
+    juce::Label inputLabel, driveLabel, lowLabel, midLabel, highLabel, outputLabel;
+    std::vector<juce::Label*> labels;
+        
+    std::string inputSliderLabelText = "Input";
+    std::string driveSliderLabelText = "Drive";
+    std::string lowSliderLabelText = "Low";
+    std::string midSliderLabelText = "Mid";
+    std::string highSliderLabelText = "High";
+    std::string outputSliderLabelText = "Output";
+    std::vector<std::string> labelTexts;
+        
+    ViatorDial customDial;
+    juce::DropShadow shadowProperties;
+    juce::DropShadowEffect dialShadow;
+    
+    DiodeAmplifierAudioProcessor& audioProcessor;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (DiodeAmplifierAudioProcessorEditor)
+};
